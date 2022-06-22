eac69d2fad67c5cac29666175e2e3c7f981b2de0
Santhosh Kumar Rajagiri <seo.santhosh@gmail.com>
2022-06-22 15:32:48 +0530

    update some data

--- data.js
+++ data.js
@@ -8,43 +8,228 @@ $(document).ready(function(){
             },
             {
                 id:"330618",
-                name:"s. v. raju",
+                name:"S V Raju",
                 doj:"13-10-2018",
                 status: "1"
             },
             {
                 id:"350997",
-                name:"e uday kumar",
+                name:"E uday kumar",
                 doj:"31-10-2018",
                 status: "1"
             },
             {
                 id:"369357",
-                name:"c. bharath",
+                name:"C Bharath",
                 doj:"14-11-2018",
                 status: "1"
             },
             {
                 id:"416541",
-                name:"b poli raju",
+                name:"B Poli Raju",
                 doj:"10-12-2018",
+                status: "1"
+            },
+            {
+                id:"568585",
+                name:"V V Vijayanad",
+                doj:"25-03-2019",
+                status: "1"
+            },
+            {
+                id:"568472",
+                name:"T Nagakarunakar",
+                doj:"25-03-2019",
+                status: "1"
+            },
+            {
+                id:"570076",
+                name:"Pradeep Kumar",
+                doj:"26-03-2019",
+                status: "1"
+            },
+            {
+                id:"778936",
+                name:"K Chanti",
+                doj:"15-07-2019",
+                status: "1"
+            },
+            {
+                id:"817553",
+                name:"Santhosh",
+                doj:"10-08-2019",
+                status: "1"
+            },
+            {
+                id:"970951",
+                name:"M Rakesh",
+                doj:"20-10-2019",
+                status: "1"
+            },
+            {
+                id:"889434",
+                name:"R Santhosh Kumar",
+                doj:"12-09-2019",
+                status: "1"
+            },
+            {
+                id:"996920",
+                name:"P Naidu Babu",
+                doj:"04-11-2019",
+                status: "1"
+            },
+            {
+                id:"1177174",
+                name:"K Divakar",
+                doj:"01-07-201",
                 status: "1"
             },
             {
                 id:"",
                 name:"",
                 doj:"",
-                status: ""
+                status: "1"
+            },
+            {
+                id:"",
+                name:"",
+                doj:"",
+                status: "1"
+            },
+            {
+                id:"",
+                name:"",
+                doj:"",
+                status: "1"
+            },
+            {
+                id:"",
+                name:"",
+                doj:"",
+                status: "1"
+            },
+            {
+                id:"",
+                name:"",
+                doj:"",
+                status: "1"
+            },
+            {
+                id:"",
+                name:"",
+                doj:"",
+                status: "1"
+            },
+            {
+                id:"",
+                name:"",
+                doj:"",
+                status: "1"
+            },
+            {
+                id:"",
+                name:"",
+                doj:"",
+                status: "1"
+            },
+            {
+                id:"",
+                name:"",
+                doj:"",
+                status: "1"
+            },
+            {
+                id:"",
+                name:"",
+                doj:"",
+                status: "1"
+            },
+            {
+                id:"",
+                name:"",
+                doj:"",
+                status: "1"
+            },
+            {
+                id:"",
+                name:"",
+                doj:"",
+                status: "1"
+            },
+            {
+                id:"",
+                name:"",
+                doj:"",
+                status: "1"
+            },
+            {
+                id:"",
+                name:"",
+                doj:"",
+                status: "1"
+            },
+            {
+                id:"",
+                name:"",
+                doj:"",
+                status: "1"
+            },
+            {
+                id:"",
+                name:"",
+                doj:"",
+                status: "1"
+            },
+            {
+                id:"",
+                name:"",
+                doj:"",
+                status: "1"
+            },
+            {
+                id:"",
+                name:"",
+                doj:"",
+                status: "1"
+            },
+            {
+                id:"",
+                name:"",
+                doj:"",
+                status: "1"
+            },
+            {
+                id:"",
+                name:"",
+                doj:"",
+                status: "1"
+            },
+            {
+                id:"",
+                name:"",
+                doj:"",
+                status: "1"
             },
            ];
            
            
-  function sortByProp(data,prop){
+  function sortByProp(data,prop,type){
     var propBySort = data.slice(0);
     
     propBySort.sort(function(a,b) {
-    var x = a[prop].toLowerCase();
-    var y = b[prop].toLowerCase();
+        
+        if(type == "num")
+        {
+            var x = parseInt(a[prop]);
+            var y = parseInt(b[prop]);
+            
+        }
+        if(type == "text")
+        {
+            var x = a[prop].toLowerCase();
+            var y = b[prop].toLowerCase();
+        }
+    
     return x < y ? -1 : x > y ? 1 : 0;
     }); 
     return (propBySort)                
@@ -52,12 +237,12 @@ $(document).ready(function(){
  
   
   var user_table,x=1;
-        user = sortByProp(user,"id")
+        user = sortByProp(user,"id","num")
         for(let i in user){
             status = `<span class=text-danger>In Active</span>`;
             if(user[i].status == 1){status = "<span class=text-success>Active</span>"}
             name = user[i].name.toUpperCase()
-            user_table += `<tr><td>${x}</td><td>${user[i].id}</td><td>${name}</td><td>${user[i].doj}</td><td>${status}</td></tr>`
+            user_table += `<tr><td>${x}</td><td class='text-end'>${user[i].id}</td><td>${name}</td><td>${user[i].doj}</td><td>${status}</td></tr>`
             x++;
             }
         $("#user").html(user_table)
